3af1e320c41ca3ba29f69410fb787782cde42b67
Artem Folinger <folingerzlive@gmail.com>
2026-08-12 19:43:42 +0300

    add conv buffer

---
+++ src/conversation_buffer.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+from . import config
+
+
+class ConversationBuffer:
+    def __init__(self, minutes: int = config.BUFFER_MINUTES):
+        self._minutes = minutes
+        self._items = []  # (timestamp, text)
+        self._lock = threading.Lock()
+
+    def append(self, text: str):
+        now = time.time()
+        with self._lock:
+            self._items.append((now, text))
+            cutoff = now - self._minutes * 60
+            while self._items and self._items[0][0] < cutoff:
+                self._items.pop(0)
+
+    def get_text(self) -> str:
+        with self._lock:
+            return " ".join(text for _, text in self._items)
